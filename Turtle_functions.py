d7521f8ecbbf7e91f33eb62611e4ff95e4c2f886
ankgomes <antoniocpmg@hotmail.de>
2020-05-27 12:26:59 -0300

    Octagon function added

--- Turtle_functions.py
+++ Turtle_functions.py
@@ -1,28 +1,34 @@
 import turtle
 
 def pentagram(x=100):
-    scr = turtle.Screen()
+    turtle.Screen()
     for _ in range(5):
         turtle.right(144)
         turtle.forward(x)
     turtle.exitonclick()
 
 def square(x):
-    scr = turtle.Screen()
+    turtle.Screen()
     for _ in range(4):
         turtle.forward(x)
         turtle.right(90)
     turtle.exitonclick()
 
 def triangle(x,y=120):
-    scr = turtle.Screen()
+    turtle.Screen()
     for _ in range(3):
         turtle.forward(x)
         turtle.left(y)
     turtle.exitonclick()
 
 def circle(x=1):
-    scr = turtle.Screen()
+    turtle.Screen()
     turtle.circle(x)
     turtle.exitonclick()
 
+def octagon(x=100):
+    turtle.Screen()
+    for _ in range(8):
+        turtle.right(45)
+        turtle.forward(x)
+    turtle.exitonclick()
